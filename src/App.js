91fae9d6f90986554a3e7a4b26474947699482ff
jwalaaa <jwala@quickwork.co>
2024-08-08 11:13:47 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from "react";
+import SurveyForm from "./components/SurveyForm";
+
+function App() {
+  return (
+    <div className="App">
+      <SurveyForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/json.js
@@ -0,0 +1,46 @@
+export const json = {
+  "title": "Sampark new form requirement",
+  "logoPosition": "right",
+  "pages": [
+    {
+      "name": "page1",
+      "elements": [
+        {
+          "type": "dropdown",
+          "name": "question1",
+          "title": "Branch\r\n",
+          "choices": [
+            "Item 1",
+            "Item 2",
+            "Item 3"
+          ]
+        },
+        {
+          "type": "dropdown",
+          "name": "question2",
+          "title": "Customer LAN ID\r\n",
+          "choices": [
+            "Item 1",
+            "Item 2",
+            "Item 3"
+          ]
+        },
+        {
+          "type": "dropdown",
+          "name": "question3",
+          "title": "Disposition\r\n",
+          "choices": [
+            "Item 1",
+            "Item 2",
+            "Item 3"
+          ]
+        },
+        {
+          "type": "text",
+          "name": "question4",
+          "title": "Remarks\r\n"
+        }
+      ]
+    }
+  ]
+}
